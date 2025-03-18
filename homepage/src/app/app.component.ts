9c09924c199b30fbdee44b382effd5dc9e7c51a8
Jenny Leidig <jennymaeleidig@gmail.com>
2025-03-18 15:06:11 -0400

    start webamp integration

--- homepage/src/app/app.component.ts
+++ homepage/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component,ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import Webamp from 'webamp';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,19 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  @ViewChild('app') app!: ElementRef<HTMLElement>
   title = 'homepage';
+
+  ngAfterViewInit() {
+    let webamp: Webamp = new Webamp({}); 
+    // Here we go! Let's show Webamp.
+    webamp.renderWhenReady(this.app.nativeElement)
+  }
+
+  ngOnInit() {
+    if (!Webamp.browserIsSupported()) {
+      alert("Oh no! Webamp does not work in this browser!");
+      throw new Error("What's the point of anything?");
+    }
+  }
 }
