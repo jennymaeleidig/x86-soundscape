b6fe0fc85133e30119592e28286aaadf843af416
Jenny Leidig <jennymaeleidig@gmail.com>
2025-03-22 12:11:51 -0400

    fix draggable icons

--- src/app/components/applet/applet.component.ts
+++ src/app/components/applet/applet.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input } from '@angular/core';
-import {CdkDragHandle} from '@angular/cdk/drag-drop';
+import { Component, EventEmitter, Inject, Input, Output } from '@angular/core';
+import { CdkDragHandle } from '@angular/cdk/drag-drop';
 import { PopUpService } from '../../services/pop-up/pop-up.service';
 import { PopUpComponent } from '../pop-up/pop-up.component';
 
@@ -8,21 +8,30 @@ import { PopUpComponent } from '../pop-up/pop-up.component';
   standalone: true,
   imports: [CdkDragHandle],
   templateUrl: './applet.component.html',
-  styleUrl: './applet.component.css'
+  styleUrl: './applet.component.css',
 })
 export class AppletComponent {
   @Input() title!: string;
   @Input() icon!: string;
-  console=console
 
-  constructor(private popUpService: PopUpService) {}
+  constructor(
+    private popUpService: PopUpService,
+    @Inject('appletIsMoving') public setAppletIsMoving: Function
+  ) {}
 
   //TODO: Adapt this to the Window component / service
-  openPopUpComponent() { 
+  openPopUpComponent() {
     this.popUpService.open(PopUpComponent, {
-      msg: "Hi",
-      callback: (() => {})
-    }
-    );
+      msg: 'Hi',
+      callback: () => {},
+    });
+  }
+
+  setMoving() {
+    this.setAppletIsMoving(true);
+  }
+
+  unsetMoving() {
+    this.setAppletIsMoving(false);
   }
 }
--- src/app/components/desktop/desktop.component.ts
+++ src/app/components/desktop/desktop.component.ts
@@ -1,8 +1,8 @@
-import { Component} from '@angular/core';
+import { Component, Injector, ViewChild } from '@angular/core';
 import { MenuComponent } from '../menu/menu.component';
-import { WinampComponent } from "../winamp/winamp.component";
-import { WindowComponent } from "../window/window.component";
-import { AppletComponent } from "../applet/applet.component";
+import { WinampComponent } from '../winamp/winamp.component';
+import { WindowComponent } from '../window/window.component';
+import { AppletComponent } from '../applet/applet.component';
 import { DragToSelectModule } from 'ngx-drag-to-select';
 import { NgComponentOutlet, NgFor } from '@angular/common';
 import { CdkDrag, CdkDragHandle } from '@angular/cdk/drag-drop';
@@ -10,36 +10,56 @@ import { CdkDrag, CdkDragHandle } from '@angular/cdk/drag-drop';
 @Component({
   selector: 'app-desktop',
   standalone: true,
-  imports: [MenuComponent, WinampComponent, WindowComponent, DragToSelectModule, NgFor, NgComponentOutlet, CdkDrag],
+  imports: [
+    MenuComponent,
+    WinampComponent,
+    WindowComponent,
+    DragToSelectModule,
+    NgFor,
+    NgComponentOutlet,
+    CdkDrag,
+  ],
   templateUrl: './desktop.component.html',
-  styleUrl: './desktop.component.css'
+  styleUrl: './desktop.component.css',
 })
 export class DesktopComponent {
   selectedApplets: Array<any> = [];
   AppletComponent = AppletComponent;
+  console = console;
+  // TODO: POssibly extract this value;
   appletInputs = [
     {
-      title: "About",
-      icon: "assets/images/Note.png"
+      title: 'About',
+      icon: 'assets/images/Note.png',
     },
     {
-      title: "Visualizer",
-      icon: "assets/images/Viz.png"
+      title: 'Visualizer',
+      icon: 'assets/images/Viz.png',
     },
     {
-      title: "Webamp",
-      icon: "assets/images/Sound.png"
+      title: 'Webamp',
+      icon: 'assets/images/Sound.png',
     },
     {
-      title: "Annoucements!",
-      icon: "assets/images/Annouce.png"
-    }
-  ]
+      title: 'Annoucements',
+      icon: 'assets/images/Annouce.png',
+    },
+  ];
+  appletIsMoving: boolean = false;
+  injector = Injector.create({
+    providers: [
+      {
+        provide: 'appletIsMoving',
+        useValue: (data: boolean) => this.setAppletIsMoving(data),
+      },
+    ],
+  });
 
-  ngOnInit(){
-  }
+  constructor() {}
 
-  itemsSelected(event:any){
-    console.log(this.selectedApplets)
+  ngOnInit() {}
+
+  setAppletIsMoving(data: boolean) {
+    this.appletIsMoving = data;
   }
 }
