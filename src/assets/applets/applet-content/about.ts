90fee45595e17121637920493e5488201b263501
jennymaeleidig <jennymaeleidig@gmail.com>
2025-06-28 17:47:33 -0400

    update about

--- src/assets/applets/applet-content/about.ts
+++ src/assets/applets/applet-content/about.ts
@@ -5,7 +5,7 @@ export interface AboutContent {
 export default class AboutInput {
   static aboutInput: AboutContent = {
     msg: `
-      Welcome to the webpage for the x86 Soundscape internet radio livestream (streaming at a crisp 128kbps). Huge shoutout to the team over at the Webamp project and sakun for the system.css library. Webpage built with the TypeScript framework Angular. Radio hosted on a Raspberry Pi Zero 2 W running mpd (music player daemon) and Icecast2.
+      Welcome to the webpage for the x86 Soundscape internet radio livestream (streaming at a crisp 192kbps). Huge shoutout to the team over at the Webamp project and sakun for the system.css library. Webpage built with the TypeScript framework Angular. Radio hosted on an Unraid server running AzuraCast and streaming via Icecast2.
     `,
   };
 }
