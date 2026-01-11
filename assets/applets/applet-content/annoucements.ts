026167ea6aa6921a7acba5dedf3ede941cd74601
jennymaeleidig <jennymaeleidig@gmail.com>
2026-01-11 16:57:40 -0500

    Auto-generated commit

--- assets/applets/applet-content/annoucements.ts
+++ assets/applets/applet-content/annoucements.ts
@@ -7,6 +7,11 @@ export interface AnnoucementContent {
 export default class AnnoucementsInput {
   static accouncementsInput: AnnoucementContent[] = [
     {
+      title: '56 kbit/s - Hiatus',
+      msg: '56 kbit/s is on pause for a while, thank you for the support ♥. Stay tuned for future updates and new music!? Some minor tweaks in here for ya as well. -- DJ x86',
+      date: '1/11/25',
+    },
+    {
       title: 'New Sights (⌐■_■)',
       msg: 'Enjoy some improved reliability and check out the new visualizer and added ambience. Stay chill. -- DJ x86',
       date: '07/10/25',
